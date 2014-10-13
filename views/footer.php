0d7944dc29f50d441cefc4d530d81e53329e3933
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 17:12:15 +0200

    added jquery as dep

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,5 @@
 
-
+    <script src="<?php echo $url; ?>/assets/js/jquery.min.js"></script>
     <script src="<?php echo $url; ?>/assets/js/bootstrap.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.17"></script>
     <script src="<?php echo $url; ?>/assets/js/sweet-alert.min.js"></script>
