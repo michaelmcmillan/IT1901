d097c98debcfffdf0bf985b12169c8810dd377c6
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 11:26:10 +0200

    remove deploy from git +x gets reset

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-wget \
-  --header='Authorization: token 9f8cf3c903e73c10218150d82a35de4abece9a67' \
-  https://api.github.com/repos/michaelmcmillan/IT1901/tarball/production \
-  -O production.tar;
-tar -xvf 'production.tar' --strip-components=1;
-rm 'production.tar';
