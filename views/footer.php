3693fb6cf1667e4f229f41597768bc1e3c14de52
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 17:13:34 +0200

    added gmaps.js

--- views/footer.php
+++ views/footer.php
@@ -2,6 +2,7 @@
     <script src="<?php echo $url; ?>/assets/js/jquery.min.js"></script>
     <script src="<?php echo $url; ?>/assets/js/bootstrap.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.17"></script>
+    <script src="<?php echo $url; ?>/assets/js/gmaps.js"></script>
     <script src="<?php echo $url; ?>/assets/js/sweet-alert.min.js"></script>
     <script src="<?php echo $url; ?>/assets/js/user.js"></script>
   </body>
