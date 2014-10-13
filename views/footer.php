86c24bc581d803fd77f41373f7165b3ce917e813
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 14:12:22 +0200

    bootstrap 3.2.0 added

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,5 @@
 
+    
+    <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -4,5 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <title>NTNUI Koiesystem</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
   </head>
   <body>
