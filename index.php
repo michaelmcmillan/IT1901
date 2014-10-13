a54c85bd52210e475895bcdbb5ff75c3f03703ae
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 14:57:07 +0200

    login handeling

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
  * Bootstrap
  */
 require 'vendor/autoload.php';
+require 'rb.php';
+require 'configuration.php';
+error_reporting(-1);
+ini_set('display_errors', 'On');
 $app = new \Slim\Slim();
 $app->config(array(
     'debug' => true,
     'templates.path' => 'views/'
 ));
+
+R::setup('mysql:host='.$config['host'].';dbname='.$config['db'],
+    $config['user'], $config['pass']);
+
 
 require 'controllers/serverController.php';
 require 'controllers/authenticationController.php';
