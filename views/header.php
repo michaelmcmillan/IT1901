fe0dd344230b3f6be95ced24c5f34843b4a7baba
Michael McMillan <email@michaelmcmillan.net>
2014-10-13 17:10:15 +0200

    added sweetalert.js

--- views/header.php
+++ views/header.php
@@ -6,6 +6,7 @@
     <title>NTNUI Koiesystem</title>
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/sweet-alert.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/style.css">
   </head>
   <body>
