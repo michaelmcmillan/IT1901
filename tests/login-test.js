a925b24eb511612457df16009a0ec7ea58bde8b1
Michael McMillan <email@michaelmcmillan.net>
2014-10-14 21:01:30 +0200

    changed the test criteria

--- tests/login-test.js
+++ tests/login-test.js
@@ -2,14 +2,10 @@
 casper.test.begin('Login and logout of a user', 1, function suit (test) {
     var server = casper.cli.get("server");
 
-    casper.start(server + '/index.php/authenticate');
-
-    /* Start the stopwatch */
-    casper.then(function () {
-        test.assertVisible('input.btn', 'Login button is present');
-    });
-
-    casper.run(function() {
+    casper.start(server + '/index.php/authenticate', function() {
+        test.assertTitleMatch(/NTNU/, 'NTNU ble funnet i tittelen');
+    }).run(function() {
         test.done();
     });
+    
 });
