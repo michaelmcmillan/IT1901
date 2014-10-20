0fb19fcccd65af0acd22ed6642c8587a76707846
Michael McMillan <email@michaelmcmillan.net>
2014-10-20 12:08:20 +0200

    reservation report is now a bs 3.2.0 modal

--- views/user.php
+++ views/user.php
@@ -25,6 +25,48 @@
         </div>
     </div>
 
+    <!-- Report a stay -->
+    <div class="modal reservation-report">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title">Modal title</h4>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-hover reservation-table">
+                              <thead>
+                                <tr>
+                                  <th>Inventar</th>
+                                  <th>Kommentar</th>
+                                  <th>Ødelagt</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                  <tr>
+                                      <td>Piano</td>
+                                      <td><input type="text" class="form-control input-xs"></td>
+                                      <td>lol</td>
+                                  </tr>
+                              </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <!-- Previous reservations list -->
     <div class="row reservation-previous" style="display:none;">
         <div class="col-md-12">
@@ -42,7 +84,6 @@
               </tbody>
             </table>
         </div>
-
     </div>
 
     <!-- Reservation form -->
