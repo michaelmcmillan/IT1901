75258e955ccd2184d47266700522809e240ca40b
Michael McMillan <email@michaelmcmillan.net>
2014-10-28 14:56:42 +0100

    added a controller for handeling adminpanel

---
+++ controllers/adminController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * View: User
+ * - Displays the user dashboard
+ */
+$app->get('/admin', $authenticateFor('admin'), function () use ($app) {
+
+    $app->render('header.php', array ('url' => $app->config('url')));
+    $app->render('admin.php',  array ('url' => $app->config('url')));
+    $app->render('footer.php', array ('url' => $app->config('url')));
+
+});
