2a998da10c569db0f2e7fa4d739927d7e7a3a357
Michael McMillan <email@michaelmcmillan.net>
2014-10-28 15:05:09 +0100

    added a view for the adminpanel

---
+++ views/admin.php
@@ -0,0 +1,46 @@
+
+<div class="container">
+
+     <!-- Static navbar -->
+      <div class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Administrer</span>
+            </button>
+            <a class="navbar-brand" href="#">Administrer</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a id="reservation-previous" href="#">Tidligere reservasjoner</a></li>
+              <li><a href="<?php echo $url?>/index.php/logout">Logg ut</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    <div class="row map">
+        <div class="col-md-12">
+            <div id="map-canvas"></div>
+        </div>
+    </div>
+
+    <!-- Previous reservations list -->
+    <div class="row reservation-previous" style="display:none;">
+        <div class="col-md-12">
+            <table class="table table-hover reservation-table">
+              <thead>
+                <tr>
+                  <th>Koie</th>
+                  <th>Fra ...</th>
+                  <th>Til</th>
+                  <th>Rapporter</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <!-- Reservations will be appended here -->
+              </tbody>
+            </table>
+        </div>
+    </div>
+</div>
