7519bd79eb3b6b04858736fbc830dd3ba53a01d4
Michael McMillan <email@michaelmcmillan.net>
2014-11-14 11:21:10 +0100

    d3 was wrongly referenced

--- views/footer.php
+++ views/footer.php
@@ -10,7 +10,7 @@
     <script src="<?php echo $url; ?>/assets/js/user.js"></script>
     <?php endif; ?>
     <?php if ($admin == true): ?>
-	<script src="<?php echo $url; ?>/assets/js/d3.min.js" charset="utf-8"></script>
+	<script src="<?php echo $url; ?>/assets/js/d3.v3.min.js" charset="utf-8"></script>
 	<script src="<?php echo $url; ?>/assets/js/admin.js"></script>
     <?php endif; ?>
   </body>
