51358f190972b20f55dce629eeb19faa5ea9ef67
Michael McMillan <email@michaelmcmillan.net>
2014-11-17 11:31:53 +0100

    included metricsgraphics

--- views/footer.php
+++ views/footer.php
@@ -6,12 +6,13 @@
     <script src="https://maps.googleapis.com/maps/api/js?v=3.17"></script>
     <script src="<?php echo $url; ?>/assets/js/gmaps.js"></script>
     <script src="<?php echo $url; ?>/assets/js/sweet-alert.min.js"></script>
-    <script src="<?php echo $url; ?>/assets/js/markerwithlabel.js"></script>
+    <script src="<?php echo $url; ?>/assets/js/markerwithlabel.js"></script>	
+    <script src="<?php echo $url; ?>/assets/js/d3.v3.min.js" charset="utf-8"></script>
+    <script src="<?php echo $url; ?>/assets/js/metricsgraphics.min.js"></script>	
     <?php if ($user == true): ?>
     <script src="<?php echo $url; ?>/assets/js/user.js"></script>
     <?php endif; ?>
     <?php if ($admin == true): ?>
-	<script src="<?php echo $url; ?>/assets/js/d3.v3.min.js" charset="utf-8"></script>
     <script src="<?php echo $url; ?>/assets/js/stats.js" charset="utf-8"></script>
 	<script src="<?php echo $url; ?>/assets/js/admin.js"></script>
     <?php endif; ?>
